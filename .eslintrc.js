43a2c269321df9bd5a0fd54189f481d1c0ccce6d
manish gupta <shwetabhgupta2002@gmail.com>
2024-06-30 11:40:28 +0530

    chnages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
-  },
+  },  
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,9 @@ describe('AppController (e2e)', () => {
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [AppModule], 
+    }).compile();  
+
 
     app = moduleFixture.createNestApplication();
     await app.init();
