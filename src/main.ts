77eb70fbfb60d47c7dc96b95c061e0a158bbcf60
manish gupta <shwetabhgupta2002@gmail.com>
2024-07-03 10:36:52 +0530

    refractor code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // console.log(app);
-  const listener = await app.listen(3000);
-  console.log(`port is listening on ${listener._connectionKey}`);
+  const listener = await app.listen(3000,()=>{
+    console.log('Port is listening on 3000')
+  });
+  // console.log(`port is listening on ${listener._connectionKey}`);
 }
 bootstrap();
